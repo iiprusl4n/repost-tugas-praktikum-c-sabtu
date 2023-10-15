8bbbdbf3e14a3835bd0af5d6cc0ffe3efb89ad3f
unknown <iipruslan1@gmail.com>
2023-10-15 09:16:08 +0700

    first commit

---
+++ Pertemuan 1/tugas mandiri 1.5 - 1 Nim Nama.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    char nim[] = "112233";
+    char nama[] = "Fauzan Fadhlurrohman Santosa";
+    char jurusan[] = "Teknik Informatika"; 
+
+    
+    printf("NIM: %s\n", nim);
+    printf("NAMA: %s\n", nama);
+    printf("JURUSAN: %s\n", jurusan);
+
+    return 0;
+}
